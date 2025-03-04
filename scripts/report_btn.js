fac33858dcaf016ca2d8f133974f65f992464a95
Joshua <90715717+Fossa88@users.noreply.github.com>
2025-03-04 12:31:00 -0800

    Add report button functionality

--- scripts/report_btn.js
+++ scripts/report_btn.js
@@ -1,4 +1,9 @@
-function reportbtn() {
+function reportbtn_open() {
         var T = document.getElementById("report_page");
-        T.style.display = "block"; 
+        T.style.display = "block";
+}
+
+function reportbtn_close() {
+        var T = document.getElementById("report_page");
+        T.style.display = "none";
 }
