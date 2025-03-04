2dbccdba692099a3f246295c29ca300e48d25f32
Joshua <90715717+Fossa88@users.noreply.github.com>
2025-03-04 12:54:00 -0800

    Add full funcitonality to report button as in the wireflow using basic js

--- scripts/report_btn.js
+++ scripts/report_btn.js
@@ -7,3 +7,22 @@ function reportbtn_close() {
         var T = document.getElementById("report_page");
         T.style.display = "none";
 }
+
+function reportbtn_confirm() {
+        var T = document.getElementById("report_page");
+        T.style.display = "none";
+        var T = document.getElementById("confirm_page");
+        T.style.display = "block";
+}
+
+function reportbtn_confirm_close() {
+        var T = document.getElementById("confirm_page");
+        T.style.display = "none";
+        var T = document.getElementById("final_page");
+        T.style.display = "block";
+}
+
+function reportbtn_final_close() {
+        var T = document.getElementById("final_page");
+        T.style.display = "none";
+}
