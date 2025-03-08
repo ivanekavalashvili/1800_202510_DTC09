20578db99821a488144ee44f741590a08a4344e2
JoNoToPo <J.engelhard0@gmail.com>
2025-03-08 14:11:44 -0800

    Implement skill dictionary

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,9 +1,9 @@
 example_dictionary = {
-    "📱 Electronics": ["Software Development", "Hardware Development"],
-    "🎨 Art & Design": ["Traditional art", "Digital art", "Pottery"],
-    "🔧 Trades": ["Machenery", "Carpentry", "etc"],
-    "🗣️ Languages": ["English", "Mandarin Chinese", "Spanish", "Hindi", "Portuguese", "Bengali", "Russian", "Japanese", "Punjabi", "Vietnamese", "Cantonese", "Korean", "French", "German",],
-    "🎵 Music": ["Producing", "Piano", "Guitar", "Violin", "Drums"]
+    "Electronics": ["Software Development", "Hardware Development"],
+    "Art": ["Traditional art", "Digital art", "Pottery"],
+    "Trades": ["Machenery", "Carpentry", "etc"],
+    "Languages": ["English", "Mandarin Chinese", "Spanish", "Hindi", "Portuguese", "Bengali", "Russian", "Japanese", "Punjabi", "Vietnamese", "Cantonese", "Korean", "French", "German",],
+    "Music": ["Producing", "Piano", "Guitar", "Violin", "Drums"]
 };
 
 function list_categories() {
@@ -17,11 +17,23 @@ function list_categories() {
             <div class="bg-white rounded-lg shadow p-6">
                 <h1 class="hover:bg-gray-50 p-2 rounded font-semibold transition"><a href="#">${Object.keys(example_dictionary)[key]}</a></h1>
                 <div class="flex items-center space-x-4">
-                    <div class="flex space-x-4 pt-4 border-t">
+                    <div class="flex space-x-4 pt-4 border-t flex-wrap" id="${Object.keys(example_dictionary)[key]}">
                     </div>
                 </div>
             </div>
-                    `)
+        `)
+        console.log(Object.keys(example_dictionary)[key])
+        let unwrapped_array = example_dictionary[Object.keys(example_dictionary)[key]]
+        
+        for (item in unwrapped_array) {
+            console.log(item)
+            $("#" + Object.keys(example_dictionary)[key]).append(`
+                <button
+                    class="bg-uranian_blue text-oxford_blue px-8 py-3 rounded-full font-semibold hover:bg-ruddy_blue hover:text-yale_blue transition duration-300 my-3">
+                    ${unwrapped_array[item]}
+                </button>
+                `)
+        }
     }
 }
 
