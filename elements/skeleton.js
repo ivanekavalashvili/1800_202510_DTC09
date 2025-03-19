f3cd7b5a936fd7281819c6e2f0454d37f894538e
Connor Brown <96628439+falconnor4@users.noreply.github.com>
2025-03-18 22:04:13 -0700

    Re-Structure re-used elements.

--- elements/skeleton.js
+++ elements/skeleton.js
@@ -1,3 +1,9 @@
-fetch('elements/chat.html')
-    .then(response => response.text())
-    .then(text => document.getElementById('elementID').innerHTML = text);
+function loadElement(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(text => document.getElementById(elementId).innerHTML = text);
+}
+
+// Load navbar and footer
+loadElement('navbar', 'elements/navbar.html');
+loadElement('footer', 'elements/footer.html');
